86f68ef24b8f741bd6436fa9ae439346b4b0892a
unknown <leeterri1219@gmail.com>
2021-11-18 22:32:01 +0200

    the demo code was changed to a function for a single call for the iris segmentation code

--- Pupil_localization_demo.py
+++ Pupil_localization_demo.py
@@ -2,16 +2,16 @@
 # This is the first step inpre-processing
 # The aim is to find the circle parameters of the pupil center
 
-from pupil_localization import *
-
-# read image
-eyeImage = readImage("aeval1.bmp");
-greyscaleImg = eyeImage[0];
-# binary image with threshold value of 0.2
-binaryImg = binImg(greyscaleImg, 0.2)[0];
-# morphological operator value set to 3px. this is the disk size
-labelledImg = turnToBlobs(binaryImg, 3);
-pupilBlobImg = returnPupilBlob(labelledImg);
-circleParam = pupilCircle(pupilBlobImg);
-
-print(circleParam)
+def pupilParam(image):
+    from pupil_localization import *
+    # read image
+    eyeImage = readImage(image);
+    greyscaleImg = eyeImage[0];
+    # binary image with threshold value of 0.2
+    binaryImg = binImg(greyscaleImg, 0.2)[0];
+    # morphological operator value set to 3px. this is the disk size
+    labelledImg = turnToBlobs(binaryImg, 3);
+    pupilBlobImg = returnPupilBlob(labelledImg);
+    circleParam = pupilCircle(pupilBlobImg);
+    
+    return circleParam
